dec67c0727d8dfccb6caad40032612ad639ec087
Lukewh <luke@lukewh.com>
2023-06-13 12:03:40 +0100

    Automate releases

---
+++ updater.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+date
+json=$(curl --silent "https://api.github.com/repos/oven-sh/bun/releases/latest")
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ -f "$BASE_DIR/.env" ]; then
+    . "$BASE_DIR/.env"
+fi
+
+cd "$BASE_DIR"
+
+current_version=$(curl -H "Snap-Device-Series: 16" --silent "https://api.snapcraft.io/v2/snaps/info/bunjs")
+current_version=$(echo $current_version | jq -r '.["channel-map"][0].version')
+
+latest_version=$(echo "$json" | jq -r 'if .prerelease == false then .tag_name else null end')
+
+echo "Current version: ${current_version}"
+echo "Latest version: ${latest_version}"
+
+if [ "$current_version" != "$latest_version" ]; then
+    echo "Update to $latest_version"
+    sed 's/\$version/'${latest_version}'/g' "_snapcraft.yaml" > "snapcraft.yaml"
+    echo "Updated"
+else
+    echo "Nothing to update"
+fi
